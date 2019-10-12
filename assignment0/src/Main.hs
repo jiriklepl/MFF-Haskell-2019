181d7d20bdadc35d772fd4d723658509976477da
Jiří Klepl <jiriklepl@seznam.cz>
2019-10-13 00:53:53 +0200

    Made a solution of the zeroth assignment

--- assignment0/src/Main.hs
+++ assignment0/src/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import System.IO
+
+main =
+    let
+        average x = fromIntegral (sum x) / fromIntegral (length x)
+        fixPoint p f = fromIntegral (round (f * 10^p)) / 10^p
+    in
+        getContents >>= print . fixPoint 2 . average . (length . words <$>) . lines
