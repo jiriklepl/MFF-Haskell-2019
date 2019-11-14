127313543114a145ebcbc6d30bda07d18852099b
jiriklepl <jiriklepl@seznam.cz>
2019-11-14 01:06:07 +0100

    added main

--- assignment2/src/Main.hs
+++ assignment2/src/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import Grammar
+import Control.Monad (void)
+import Control.Monad.Combinators.Expr -- from parser-combinators
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    contents <- getContents
+    case parse program "" contents of
+      Right a -> print a
+      Left e -> print e >> fail "parse error"
