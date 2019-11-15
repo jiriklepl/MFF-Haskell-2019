5bf719914629e62ff8b096fa484c115acaa6f03b
jiriklepl <jiriklepl@seznam.cz>
2019-11-15 21:01:27 +0100

    modified main to pretty print

--- assignment2/src/Main.hs
+++ assignment2/src/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
-import Grammar
 import Control.Monad (void)
 import Control.Monad.Combinators.Expr -- from parser-combinators
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
+import Grammar
+import Pretty
+
 main :: IO ()
 main = do
     contents <- getContents
     case parse program "" contents of
-      Right a -> print a
+      Right a -> (putStrLn . ppshow) a
       Left e -> print e >> fail "parse error"
