2e18cc38f06a39699016d29bfcf7105bad67381e
jiriklepl <jiriklepl@seznam.cz>
2019-11-15 21:00:47 +0100

    added grammar + fixes to it

--- assignment2/src/Grammar.hs
+++ assignment2/src/Grammar.hs
@@ -10,35 +10,45 @@ import qualified Text.Megaparsec.Char.Lexer as L
 import AST
 import Parser
 
-program = between sc eof (statement 0)
+program = do
+    (IStmt stmts) <- indentStatement [-1]
+    eof
+    return $ Program stmts
 
-ccStatement :: Int -> Parser (Expression, Statement)
+ccStatement :: [Int] -> Parser (Expression, Statement)
 ccStatement indent = do
     cond <- expression
     void (symbol ":")
     stmt <- statement indent
     return (cond, stmt)
 
-ifStatement :: Int -> Parser Statement
+ifStatement :: [Int] -> Parser Statement
 ifStatement indent = do
     rword "if"
     (expr, stmt) <- ccStatement indent
     return $ CStmt (IfStmt expr stmt)
 
-whileStatement :: Int -> Parser Statement
+elifStatement :: [Int] -> Parser Statement
+elifStatement indent = do
+    rword "else"
+    rword "if"
+    (expr, stmt) <- ccStatement indent
+    return $ CStmt (ElifStmt expr stmt)
+
+whileStatement :: [Int] -> Parser Statement
 whileStatement indent = do
     rword "while"
     (expr, stmt) <- ccStatement indent
     return $ CStmt (WhileStmt expr stmt)
 
-elseStatement :: Int -> Parser Statement
+elseStatement :: [Int] -> Parser Statement
 elseStatement indent = do
     rword "else"
     void (symbol ":")
     stmt <- statement indent
     return $ CStmt (ElseStmt stmt)
 
-funDefinition :: Int -> Parser Statement
+funDefinition :: [Int] -> Parser Statement
 funDefinition indent = do
     rword "def"
     fcall <- funCall
@@ -47,12 +57,15 @@ funDefinition indent = do
     return $ DStmt (FunDef fcall stmt)
 
 expression :: Parser Expression
-expression = funExpression
-    <|> binExpression
+expression = try binExpression
+    <|> simpleExpression
+
+simpleExpression :: Parser Expression
+simpleExpression = parExpression
     <|> strExpression
     <|> numExpression
+    <|> try funExpression
     <|> idExpression
-    <|> parExpression
 
 funExpression :: Parser Expression
 funExpression = FCExpr <$> funCall
@@ -68,40 +81,64 @@ numExpression = NLExpr <$> integer
 
 binExpression :: Parser Expression
 binExpression = do
-    expr1 <- expression
-    sc
-    symbol <- oneOf "+-*/"
+    expr1 <- simpleExpression
+    symbol <- binOperator
     expr2 <- expression
-    return $ case symbol of
-        '+' -> BOExpr expr1 Plus expr2
-        '-' -> BOExpr expr1 Minus expr2
-        '*' -> BOExpr expr1 Mul expr2
-        '/' -> BOExpr expr1 Div expr2
+    return $ BOExpr expr1 (BinOp symbol) expr2
+
+binOperator :: Parser String
+binOperator = foldl (<|>) empty (symbol <$> ["+", "-", "*", "/", "=", "<=", ">=", "<", ">"])
 
 parExpression :: Parser Expression
 parExpression = parens expression
 
-statement :: Int -> Parser Statement
-statement indent = ifStatement indent
-        <|> whileStatement indent
-        <|> elseStatement indent
-        <|> funDefinition indent
+inlineStatement :: [Int] -> Parser Statement
+inlineStatement indent = ifStatement indent
+    <|> whileStatement indent
+    <|> funDefinition indent
+    <|> try (elifStatement indent)
+    <|> elseStatement indent
+    <|> exprStatement
 
-argumentList :: String -> Parser [String]
-argumentList ident =
-    (do
-        void (symbol ")")
-        return [ident])
-    <|> (do
+nlStatement :: [Int] -> Parser Statement
+nlStatement indent = do
+    nl
+    (many . try) (sc >> nl)
+    indent2 <- indentifier
+    if indent2 == head indent
+        then inlineStatement indent
+        else fail  $ show indent2 ++ " vs " ++ show indent
+
+statement :: [Int] -> Parser Statement
+statement indent = (nl >> (many . try) (sc >> nl) >> indentStatement indent)
+    <|> inlineStatement indent
+
+exprStatement :: Parser Statement
+exprStatement = EStmt <$> expression
+
+indentStatement :: [Int] -> Parser Statement
+indentStatement indent = do
+    indent2 <- indentifier
+    case compare indent2 $ head indent of
+        GT -> do
+            stmt <- inlineStatement (indent2:indent)
+            stmts <- many . try $ nlStatement (indent2:indent)
+            return $ IStmt (stmt:stmts)
+        _ -> fail "indentation expected"
+
+argumentList :: Parser [Expression]
+argumentList =
+    try (do
+        ident <- expression
         void (symbol ",")
-        ident1 <- identifier
-        args <- argumentList ident1
+        args <- argumentList
         return (ident : args))
+    <|> (do
+        ident <- expression
+        return [ident])
 
 funCall :: Parser FunctionCall
 funCall = do
-    ident <- identifier
-    void (symbol "(")
-    ident1 <- identifier
-    args <- argumentList ident1
+    ident <- idExpression
+    args <- parens (try argumentList <|> pure [])
     return $ FunCall ident args
