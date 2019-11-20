af711ae192b0aa8a6312089f11fb8ba3e1a1b2be
jiriklepl <jiriklepl@seznam.cz>
2019-11-20 01:12:11 +0100

    style & removing useless code

--- assignment2/src/ParserMonad.hs
+++ assignment2/src/ParserMonad.hs
@@ -2,21 +2,17 @@ module ParserMonad where
 
 import ErrorMessage
 
-data IdentType = IdentFunction
-  | IdentVariable
-
 enterScope parserM@ParserMonad{idents = ids} = parserM{idents = []:ids}
 leaveScope parserM@ParserMonad{idents = ids} = parserM{idents = tail ids}
 
 
 isDefined id parserM@ParserMonad{idents = ids} = 
-  let isElem _ [] = False
-      isElem id (some:rest) = 
-        elem id some || isElem id rest
-  in isElem id ids
+    let isElem _ [] = False
+        isElem id (some:rest) = elem id some || isElem id rest
+    in isElem id ids
 
 data ParserMonad = ParserMonad
-  { indents :: [Int]
-  , idents :: [[String]]
-  , errorReport :: [ErrorMessage]
-  }
+    { indents :: [Int]
+    , idents :: [[String]]
+    , errorReport :: [ErrorMessage]
+    }
