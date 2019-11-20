2c00d62135681987a092287852b45174ce6d72d0
jiriklepl <jiriklepl@seznam.cz>
2019-11-20 05:35:06 +0100

    added reading from files stated as arguments

--- assignment2/src/Main.hs
+++ assignment2/src/Main.hs
@@ -5,16 +5,26 @@ import Control.Monad.Combinators.Expr -- from parser-combinators
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import System.Environment
+import System.IO
 
 import Grammar
 import Pretty
 
 main :: IO ()
-main = do
-    contents <- getContents
-    case parse program "" contents of
-        Right a -> do
-            (putStrLn . ppshow . getReport) (snd a)
-            putStrLn "code starts here:"
-            (putStrLn . ppshow) (fst a)
-        Left e -> print e >> fail "parse error"
+main = let
+    parseFile arg = do
+        contents <- if arg == "-"
+            then getContents
+            else do
+                handle <- openFile arg ReadMode
+                hGetContents handle
+        case parse program arg contents of
+            Right a -> do
+                (putStrLn . ppshow . getReport) (snd a)
+                putStrLn "code starts here:"
+                (putStrLn . ppshow) (fst a)
+            Left e -> print e >> fail "parse error"
+    in do
+        args <- getArgs
+        sequence_ (parseFile <$> args)
